452d19cc8bd1a1c4076e78a0837e5cf7d3815b2d
Kevin Giszewski <kevin@giszewski.com>
2014-01-13 10:30:43 -0500

    Setup Models and PVC/Extensions

---
+++ app/Umbraco/Umbraco.Archetype/Models/Fieldset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archetype.Umbraco.Models
+{
+    public class Fieldset
+    {
+        public string Alias { get; set; }
+        public IEnumerable<Property> Properties;
+
+        public Fieldset()
+        {
+            Properties = new List<Property>();
+        }
+
+    }
+}
