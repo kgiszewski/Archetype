f05a67d9d2b6a74ec30473fb0c4f97c6decba89a
Kevin Giszewski <kevin@giszewski.com>
2014-02-20 11:07:20 -0500

    Expose the Datatype Service GetById

---
+++ app/Umbraco/Umbraco.Archetype/Api/ArchetypeDataTypeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using AutoMapper;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models.ContentEditing;
+using Umbraco.Web.Models.Mapping;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Editors;
+
+namespace Archetype.Umbraco.Api
+{
+    [PluginController("ArchetypeApi")]
+    public class ArchetypeDataTypeController : UmbracoAuthorizedJsonController
+    {
+        //pulled from the Core
+        public DataTypeDisplay GetById(int id)
+        {
+            var dataType = Services.DataTypeService.GetDataTypeDefinitionById(id);
+            if (dataType == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return Mapper.Map<IDataTypeDefinition, DataTypeDisplay>(dataType);
+        }
+    } 
+}
