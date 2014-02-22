24bfef64dd47c6d19ea3f839244be31b68af8a8d
Tom Fulton <tomnf1@gmail.com>
2014-02-21 22:36:27 -0700

    Misc housekeeping

--- app/Umbraco/Umbraco.Archetype/Api/ArchetypeDataTypeController.cs
+++ app/Umbraco/Umbraco.Archetype/Api/ArchetypeDataTypeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Web.Http;
 using AutoMapper;
 using Umbraco.Core.Models;
 using Umbraco.Web.Models.ContentEditing;
-using Umbraco.Web.Models.Mapping;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.Editors;
 
@@ -15,7 +11,6 @@ namespace Archetype.Umbraco.Api
     [PluginController("ArchetypeApi")]
     public class ArchetypeDataTypeController : UmbracoAuthorizedJsonController
     {
-        //pulled from the Core
         public object GetById(int id)
         {
             var dataType = Services.DataTypeService.GetDataTypeDefinitionById(id);
@@ -26,5 +21,5 @@ namespace Archetype.Umbraco.Api
             var dataTypeDisplay = Mapper.Map<IDataTypeDefinition, DataTypeDisplay>(dataType);
             return new { selectedEditor = dataTypeDisplay.SelectedEditor, preValues = dataTypeDisplay.PreValues };
         }
-    } 
+    }
 }
