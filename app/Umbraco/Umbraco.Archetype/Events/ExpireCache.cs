15a242ab9ba593a6b8169cd790f4190cdbfd1e41
Tom Fulton <tomnf1@gmail.com>
2014-03-30 21:14:51 -0600

    Clear the PreValue cache when DataTypes are saved

---
+++ app/Umbraco/Umbraco.Archetype/Events/ExpireCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+
+namespace Archetype.Umbraco.Events
+{
+    public class ExpireCache : ApplicationEventHandler
+    {
+
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+            
+            DataTypeService.Saved += ExpirePreValueCache;
+        }
+
+        void ExpirePreValueCache(IDataTypeService sender, global::Umbraco.Core.Events.SaveEventArgs<IDataTypeDefinition> e)
+        {
+            ApplicationContext.Current.ApplicationCache.RuntimeCache.ClearCacheItem("Archetype_GetArchetypePreValueFromDataTypeId_" + e.SavedEntities.First().Id);
+            ApplicationContext.Current.ApplicationCache.RuntimeCache.ClearCacheItem("Archetype_GetArchetypePreValueFromDataTypeId_GetPropertyEditorAlias_"+ e.SavedEntities.First().Id);
+        }
+    }
+}
