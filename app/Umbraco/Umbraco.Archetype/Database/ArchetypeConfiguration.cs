53ed92555568a3d5e4cc3a8165cdb0260aca6ab5
kjac <kja@illumi.dk>
2014-04-30 11:33:48 +0200

    Replace dependency on Umbraco prevalue
    As described in issue #34 (imulus#34) the Archetype configuration
    quickly exceeds the 2500 characters allowed by Umbraco prevalues.
    This branch is for implementing an attempt to move the Archetype
    configuration to a separate custom table within the Umbraco database and
    solely use the Umbraco prevalue as an identifier to find the Archetype
    configuration in the new table.

---
+++ app/Umbraco/Umbraco.Archetype/Database/ArchetypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace Archetype.Umbraco.Database
+{
+    [TableName("Archetype")]
+    [PrimaryKey("Id", autoIncrement = false)]
+    [ExplicitColumns]
+    public class ArchetypeConfiguration
+    {
+        [Column("Id")]
+        [PrimaryKeyColumn(AutoIncrement = false)]
+        public Guid Id { get; set; }
+
+        [Column("Configuration")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string Configuration { get; set; }
+    }
+}
