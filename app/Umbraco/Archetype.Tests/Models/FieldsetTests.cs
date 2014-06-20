2f195dd588878d65aae35c645e9b5355fe29a543
kjac <kja@illumi.dk>
2014-06-20 10:58:21 +0200

    Add default value overload to GetValue()
    Fix issue #142

--- app/Umbraco/Archetype.Tests/Models/FieldsetTests.cs
+++ app/Umbraco/Archetype.Tests/Models/FieldsetTests.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Archetype.PropertyConverters;
 using NUnit.Framework;
+using Archetype.Tests.Serialization;
 
 namespace Archetype.Tests.Models
 {
@@ -34,6 +35,17 @@ namespace Archetype.Tests.Models
             Assert.That(propertyValue == "Box 1 Title");
         }
 
+		[Test]
+		public void Can_Get_Fieldset_Property_Default_Value_By_Alias() {
+			var converter = new ArchetypeValueConverter();
+			var result = (Archetype.Models.ArchetypeModel)converter.ConvertDataToSource(null, _sampleJson, false);
+
+			var fieldset = result.Fieldsets.First();
+			var propertyValue = fieldset.GetValue<string>("noSuchProperty", "noSuchProperty default value");
+
+			Assert.That(propertyValue == "noSuchProperty default value");
+		}
+
         [Test]
         public void Can_Convert_Property_Value_Types()
         {
@@ -46,6 +58,23 @@ namespace Archetype.Tests.Models
             Assert.That(fieldset.GetValue<bool>("show") == true);
             Assert.That(fieldset.GetValue<string>("blurb") == "A blurb here");
         }
+
+		[Test]
+		public void Can_Convert_Property_Default_Value_Types() {
+			var converter = new ArchetypeValueConverter();
+			var result = (Archetype.Models.ArchetypeModel)converter.ConvertDataToSource(null, _sampleJson, false);
+
+			var fieldset = result.Fieldsets.First();
+
+			Assert.That(fieldset.GetValue<int>("noSuchInteger", 1234) == 1234);
+			Assert.That(fieldset.GetValue<bool>("noSuchBoolean", true) == true);
+			Assert.That(fieldset.GetValue<bool>("noSuchBoolean", false) == false);
+			Assert.That(fieldset.GetValue<string>("noSuchString", "noSuchString default value") == "noSuchString default value");
+
+			var contactDetails = fieldset.GetValue<ContactDetails>("noSuchContact", new ContactDetails { Address = "some street, some city", Name = "someone" });
+			Assert.That(contactDetails.Address == "some street, some city");
+			Assert.That(contactDetails.Name == "someone");
+		}
 
         [Test]
         public void Returns_String_When_No_Type_Specified()
