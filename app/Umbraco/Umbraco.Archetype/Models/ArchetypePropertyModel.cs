07750c75371d04dc00efa28c77f6c18c89f43b7e
Jeroen Breuer <jbreuer@gmail.com>
2014-08-05 13:17:34 +0200

    Run ConvertDataToSource before ConvertSourceToObject

--- app/Umbraco/Umbraco.Archetype/Models/ArchetypePropertyModel.cs
+++ app/Umbraco/Umbraco.Archetype/Models/ArchetypePropertyModel.cs
@@ -67,11 +67,18 @@ namespace Archetype.Models
             foreach (var converter in converters.Where(x => x.IsConverter(properyType)))
             {
                 // Convert the type using a found value converter
-                var value2 = converter.ConvertSourceToObject(properyType, value, false);
+                var value2 = converter.ConvertDataToSource(properyType, value, false);
 
                 // If the value is of type T, just return it
                 if (value2 is T)
                     return Attempt<T>.Succeed((T)value2);
+
+                // If ConvertDataToSource failed try ConvertSourceToObject.
+                var value3 = converter.ConvertSourceToObject(properyType, value, false);
+
+                // If the value is of type T, just return it
+                if (value3 is T)
+                    return Attempt<T>.Succeed((T)value3);
 
                 // Value is not final value type, so try a regular type conversion aswell
                 var convertAttempt = value2.TryConvertTo<T>();
