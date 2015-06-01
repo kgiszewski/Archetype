d026af9fd2e2a9feb41307cab070ca9a7172c9c2
kgiszewski <kevin@giszewski.com>
2015-06-01 13:49:34 -0400

    Expose archetypeCacheService on controller

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,4 +1,4 @@
-﻿angular.module("umbraco").controller("Imulus.ArchetypeController", function ($scope, $http, assetsService, angularHelper, notificationsService, $timeout, entityResource, archetypeService, archetypeLabelService) {
+﻿angular.module("umbraco").controller("Imulus.ArchetypeController", function ($scope, $http, assetsService, angularHelper, notificationsService, $timeout, entityResource, archetypeService, archetypeLabelService, archetypeCacheService) {
 
     //$scope.model.value = "";
     $scope.model.hideLabel = $scope.model.config.hideLabel == 1;
@@ -25,7 +25,8 @@
     //hold references to helper services 
     $scope.services = {
         archetypeService: archetypeService,
-        archetypeLabelService: archetypeLabelService
+        archetypeLabelService: archetypeLabelService,
+        archetypeCacheService: archetypeCacheService
     }
 
     //helper to get $eval the labelTemplate
