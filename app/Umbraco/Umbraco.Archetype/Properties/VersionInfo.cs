8610f69c8808ff4bca00cb714d8575bd63275223
kgiszewski <3flowers>
2015-06-04 13:26:19 -0400

    Bump version to v1.8

--- app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
+++ app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.7.5")]
-[assembly: AssemblyFileVersion("1.7.5")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8")]
