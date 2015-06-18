a2121cd8a742639d21701df8b1b76279847c6b4e
leekelleher <leekelleher@gmail.com>
2015-06-18 12:17:44 +0100

    Upgraded Courier assemblies.
    Amended references to Courier constants (seems that there was some restructuring within Courier)

--- app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
+++ app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.8")]
-[assembly: AssemblyFileVersion("1.8")]
+[assembly: AssemblyVersion("1.8.1")]
+[assembly: AssemblyFileVersion("1.8.1")]
