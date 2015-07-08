cfe014ee459b715c3e4049098217053d90d1dfd4
kgiszewski <kevin@giszewski.com>
2015-07-08 13:26:06 -0400

    Bump version number

--- app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
+++ app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.8.1")]
-[assembly: AssemblyFileVersion("1.8.1")]
+[assembly: AssemblyVersion("1.9")]
+[assembly: AssemblyFileVersion("1.9")]
