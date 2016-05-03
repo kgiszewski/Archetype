dc3c1a72a8d9631f8bb84f32ca011dc6fe21a1f7
kgiszewski <kevin@giszewski.com>
2016-05-03 10:11:13 -0400

    Add some null checks

--- app/Umbraco/Umbraco.Archetype/PropertyEditors/ArcheTypePropertyEditor.cs
+++ app/Umbraco/Umbraco.Archetype/PropertyEditors/ArcheTypePropertyEditor.cs
@@ -95,7 +95,7 @@ namespace Archetype.PropertyEditors
 							if(propDef == null || propDef.DataTypeGuid == null) continue;
 							var dtd = ArchetypeHelper.Instance.GetDataTypeByGuid(Guid.Parse(propDef.DataTypeGuid));
 							var propType = new PropertyType(dtd) {Alias = propDef.Alias};
-							var prop = new Property(propType, propDef.Value.ToString());
+							var prop = new Property(propType, (propDef.Value != null) ? propDef.Value.ToString() : null);
 							var propEditor = PropertyEditorResolver.Current.GetByAlias(dtd.PropertyEditorAlias);
 							propDef.Value = propEditor.ValueEditor.ConvertDbToString(prop, propType, dataTypeService);
 						}
@@ -135,7 +135,7 @@ namespace Archetype.PropertyEditors
 						{
 							var dtd = ArchetypeHelper.Instance.GetDataTypeByGuid(Guid.Parse(propDef.DataTypeGuid));
 							var propType = new PropertyType(dtd) {Alias = propDef.Alias};
-							var prop = new Property(propType, propDef.Value.ToString());
+                            var prop = new Property(propType, (propDef.Value != null) ? propDef.Value.ToString() : null);
 							var propEditor = PropertyEditorResolver.Current.GetByAlias(dtd.PropertyEditorAlias);
 							propDef.Value = propEditor.ValueEditor.ConvertDbToEditor(prop, propType, dataTypeService);
 						}
@@ -215,7 +215,7 @@ namespace Archetype.PropertyEditors
 							var propData = new ContentPropertyData(propDef.Value, preValues, additionalData);
 							var propEditor = PropertyEditorResolver.Current.GetByAlias(dtd.PropertyEditorAlias);
 							// make sure to send the current property value (if any) to the PE ValueEditor
-							propDef.Value = propEditor.ValueEditor.ConvertEditorToDb(propData, currentProperty != null ? currentProperty.Value.ToString() : null);
+                            propDef.Value = propEditor.ValueEditor.ConvertEditorToDb(propData, currentProperty != null ? (currentProperty.Value != null) ? currentProperty.Value.ToString() : null : null);
 						}
 						catch (Exception ex)
 						{
