5aa16a663642564480be509406314cc1a51f79e4
Kenn Jacobsen <kja@vertica.dk>
2017-08-18 08:10:43 +0200

    Bump package version

--- app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
+++ app/Umbraco/Umbraco.Archetype/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.14.1")]
-[assembly: AssemblyFileVersion("1.14.1")]
+[assembly: AssemblyVersion("1.15.1")]
+[assembly: AssemblyFileVersion("1.15.1")]
